c8675f35cf2e99dac5952c0f081479cbcb496361
DimKFPS <55626527+DimKFPS@users.noreply.github.com>
2022-05-09 07:10:45 +0700

    Added a folder and file

---
+++ SOS-LED-v1/SOS-LED-v1.ino
@@ -0,0 +1,65 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+// The Letter S
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+
+// The Letter O
+digitalWrite(13, HIGH);
+delay(1000);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(1000);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(1000);
+
+digitalWrite(13, LOW);
+delay(300);
+
+// The Letter S
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+
+digitalWrite(13, HIGH);
+delay(300);
+
+digitalWrite(13, LOW);
+delay(300);
+}
