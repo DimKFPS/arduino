e04df0ffdd126198e5fff622addee9c484d4af88
DimKFPS <55626527+DimKFPS@users.noreply.github.com>
2022-05-09 07:23:40 +0700

    Add files via upload

--- SOS-LED-v1/SOS-LED-v1.ino
+++ SOS-LED-v1/SOS-LED-v1.ino
@@ -1,65 +1,63 @@
+// variables declaration (you may edit these instead)
+int pinLED = 13; // The currently assigned pin to the LED
+int offDelay = 100; // The time LED will be off
+int shortDelay = 300; // The time LED will be on in a short time
+int longDelay = 600; // The time LED will be on in a longer time
+int endDelay = 1000; // The time LED will be off after the cycle ends
+
 void setup() {
   // put your setup code here, to run once:
-pinMode(13, OUTPUT);
+pinMode(pinLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 // The Letter S
-digitalWrite(13, HIGH);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, HIGH);
-delay(300);
-
-digitalWrite(13, LOW);
-delay(300);
-
-digitalWrite(13, HIGH);
-delay(300);
-
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
 // The Letter O
-digitalWrite(13, HIGH);
-delay(1000);
+digitalWrite(pinLED, HIGH);
+delay(longDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(longDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, HIGH);
-delay(1000);
-
-digitalWrite(13, LOW);
-delay(300);
-
-digitalWrite(13, HIGH);
-delay(1000);
-
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(longDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
 // The Letter S
-digitalWrite(13, HIGH);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(offDelay);
 
-digitalWrite(13, HIGH);
-delay(300);
-
-digitalWrite(13, LOW);
-delay(300);
-
-digitalWrite(13, HIGH);
-delay(300);
-
-digitalWrite(13, LOW);
-delay(300);
+digitalWrite(pinLED, HIGH);
+delay(shortDelay);
+digitalWrite(pinLED, LOW);
+delay(endDelay);
 }
